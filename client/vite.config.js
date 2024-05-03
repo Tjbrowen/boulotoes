ebba33354498365d16e63674fa47e3dd3a56c099
Thabo Jimmy Browen <142535245+Tjbrowen@users.noreply.github.com>
2024-05-03 16:12:52 +0200

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'; // Import defineConfig from vite
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Other build options...
+    rollupOptions: {
+      // Other Rollup options...
+      external:['express-async-handler', '@prisma/client']
+    }
+  }
+});
