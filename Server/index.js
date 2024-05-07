384f70d5afa246a2be0c1b3cd892aa72f65aa3e8
Thabo Jimmy Browen <142535245+Tjbrowen@users.noreply.github.com>
2024-05-07 19:48:14 +0200

    modified index.html

--- Server/index.js
+++ Server/index.js
@@ -1,31 +1,23 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { userRoute } from "./routes/userRoute.js";
+import { servicesRoute } from "./routes/servicesRoute.js";
+import { projectsRoute } from "./routes/projetsRoute.js";
+dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(require('cookie-parser')());
-app.use(require('cors')());
-
-// Define routes
-app.get('/api/data', async (req, res) => {
-  const data = await prisma.someModel.findMany();
-  res.json(data);
-});
-
-// Import and use existing routes
-const { userRoute } = require("./routes/userRoute.js");
-const { servicesRoute } = require("./routes/servicesRoute.js");
-const { projectsRoute } = require("./routes/projectsRoute.js");
-
-app.use("/api/user", userRoute);
-app.use("/api/services", servicesRoute);
-app.use("/api/projects", projectsRoute);
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
+app.use("/api/user", userRoute);
+app.use("/api/services", servicesRoute);
+app.use("/api/projects", projectsRoute);
