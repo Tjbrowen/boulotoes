de200384fcc42c0234e6e83f05facd4737fea235
Thabo Jimmy Browen <142535245+Tjbrowen@users.noreply.github.com>
2024-05-08 07:34:52 +0200

    modified:   Server/config/prismaCofig.js

--- Server/config/prismaCofig.js
+++ Server/config/prismaCofig.js
@@ -1,5 +1,9 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const { PrismaClient } = require('@prisma/client');
+
 
 const prisma = new PrismaClient();
 
-export { prisma }
+export { prisma };
